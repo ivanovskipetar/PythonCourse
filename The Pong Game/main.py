9b5199cc96fe640c59294372082c1a3f04d9731d
Petar <ivanovskipetar4@gmail.com>
2023-11-28 20:20:11 +0100

    Pong Game-Score keeping and changing the ball speed

--- The Pong Game/main.py
+++ The Pong Game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,16 +13,17 @@ screen.tracer(0)
 left_paddle = Paddle((-350, 0))
 right_paddle = Paddle((350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(right_paddle.go_up, "Up")
-screen.onkey(right_paddle.go_down, "Down")
-screen.onkey(left_paddle.go_up, "w")
-screen.onkey(left_paddle.go_down, "s")
+screen.onkeypress(right_paddle.go_up, "Up")
+screen.onkeypress(right_paddle.go_down, "Down")
+screen.onkeypress(left_paddle.go_up, "w")
+screen.onkeypress(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -36,8 +38,11 @@ while game_is_on:
     # Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect when left paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
 screen.exitonclick()
