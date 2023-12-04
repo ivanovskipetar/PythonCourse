1781c7023e556349db015a42973d4d0842fd5548
Petar <ivanovskipetar4@gmail.com>
2023-12-04 17:27:13 +0100

    Turtle Crossing - Detect when the player collides with a car and detect when player has reached the other side

--- Turtle Crossing Capstone Project/car_manager.py
+++ Turtle Crossing Capstone Project/car_manager.py
@@ -10,6 +10,7 @@ class CarManager(Turtle):
 
     def __init__(self):
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
         random_chance = random.randint(1, 6)
@@ -23,4 +24,7 @@ class CarManager(Turtle):
 
     def move_cars(self):
         for car in self.all_cars:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward(self.car_speed)
+
+    def speed_up(self):
+        self.car_speed += MOVE_INCREMENT
