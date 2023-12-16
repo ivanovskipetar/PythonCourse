ce34c23f272b466fe3a7f1103c39b463fa9c4775
Petar <ivanovskipetar4@gmail.com>
2023-12-16 11:08:14 +0100

    Password Manager - Generate password and copy to clipboard(FINAL)

--- Password Manager GUI App/main.py
+++ Password Manager GUI App/main.py
@@ -1,10 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    letters_list = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    symbols_list = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    numbers_list = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+
+    password_list = letters_list + symbols_list + numbers_list
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    # Copy password to clipboard
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def add_data():
     website = website_entry.get()
@@ -58,7 +83,7 @@ password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=44, command=add_data)
