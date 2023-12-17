6348c72a5f21c85b3e5916e29b83c704e8f7b7e5
Petar <ivanovskipetar4@gmail.com>
2023-12-17 18:29:29 +0100

    Password Manager - Handling exceptions

--- Password Manager GUI App/main.py
+++ Password Manager GUI App/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -35,18 +36,30 @@ def add_data():
     website = website_entry.get()
     email_username = email_username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email_username,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0 or len(email_username) == 0:
         messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered:\nWebsite: {website}\nEmail/Username: {email_username}\nPassword: {password}\nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email_username} | {password}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data.
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data.
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
-            website_entry.focus()
-            email_username_entry.delete(0, END)
             password_entry.delete(0, END)
 
 
