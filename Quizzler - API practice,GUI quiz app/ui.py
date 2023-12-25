88e0c3fcfc5a7b07b877dfaf8e69c21854ecf5c4
Petar <ivanovskipetar4@gmail.com>
2023-12-25 11:27:27 +0100

    Quizzler - Give feedback to player & keeping score

--- Quizzler - API practice,GUI quiz app/ui.py
+++ Quizzler - API practice,GUI quiz app/ui.py
@@ -13,9 +13,8 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
-        self.score = 0
 
-        self.score_label = Label(text=f"Score:{self.score}", fg="white", bg=THEME_COLOR)
+        self.score_label = Label(text=f"Score:{self.quiz.score}", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
@@ -30,10 +29,10 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         wrong_img = PhotoImage(file="./images/false.png")
-        self.button_wrong = Button(image=wrong_img, highlightthickness=0)
+        self.button_wrong = Button(image=wrong_img, highlightthickness=0, command=self.true_click)
         self.button_wrong.grid(row=2, column=1)
         right_img = PhotoImage(file="./images/true.png")
-        self.button_right = Button(image=right_img, highlightthickness=0)
+        self.button_right = Button(image=right_img, highlightthickness=0, command=self.false_click)
         self.button_right.grid(row=2, column=0)
 
         self.get_next_question()
@@ -41,5 +40,26 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.q_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score:{self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.q_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.q_text, text="You've reached the end of the quiz.")
+            self.button_right.config(state="disabled")
+            self.button_wrong.config(state="disabled")
+
+    def true_click(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_click(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
